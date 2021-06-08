baafcac2e3379e5d861b12992b3a2e1eef77329f
hitesh05 <hitesh.goel05@gmail.com>
2021-06-08 12:15:31 +0530

    Corrections made. Code working.

--- Assignment_1/q4.sh
+++ Assignment_1/q4.sh
@@ -2,50 +2,31 @@
 
 clear
 
-#sed -e "$(sed 's:.*:s/&//ig:' stopwords.txt)" input.txt > out.txt
-#grep -ioFwvf stopwords.txt input.txt > out.txt
-#cat out.txt > input.txt
-#cat input.txt
-#rm -rf out.txt
-
-# for username in $(<"stopwords.txt")
-# do
-# {
-#    sed -i '/\b$username\b/d' input.txt
-# }
-# done
-
 filename="stopwords.txt"
-# while read -r line; do
-# {
-#     #name=$line
-#     sed -i '/\b$line\b/d' input.txt
-# }
-# done < "$filename"
-
-# cat input.txt
 
 while IFS= read -r line; do
 {
    name="$line"
-   sed -i "/\b$name\b/d" input.txt
+   sed -i "s/\b$name\b//gI" input.txt
 }
 done < "$filename"
 
 
 words=$(wc -w < input.txt)
+echo $words
 
 lines=$(wc -l < input.txt)
-((lines=lines+1))
+#((lines=lines+1))
+echo $lines
 
 d=`echo "($words/$lines)" | bc -l`
 
-#echo $d
+echo $d
 
 freq=`grep -o $1 input.txt | wc -l`
-#echo $freq
+echo $freq
 
 ans=`echo "($freq/$d)" | bc -l`
-#echo $ans
+echo $ans
 
 echo "sentence_withnostopwords," $ans > Output.txt
