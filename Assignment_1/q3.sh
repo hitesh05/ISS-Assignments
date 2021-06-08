636d8cd1ccd1d28381abccb373ae3c1cf9f4a11d
hitesh05 <hitesh.goel05@gmail.com>
2021-06-08 12:01:07 +0530

    ignored upper case

--- Assignment_1/q3.sh
+++ Assignment_1/q3.sh
@@ -8,13 +8,13 @@ file=$1
 
 #words=$(wc -w < $file)
 
-grep -owi '\bs[^a]\w*\b' $file
+grep -ow '\bs[^a]\w*\b' $file
 echo
 
-grep -owi '\bwh\w*\b' $file
+grep -ow '\bwh\w*\b' $file
 echo
 
-grep -owi '\bth\w*\b' $file
+grep -ow '\bth\w*\b' $file
 echo
 
-grep -owi '\ba[^n][a-zA-z]*\b' $file
+grep -ow '\ba[^n][a-zA-z]*\b' $file
