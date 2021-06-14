eaa91f26ea2a62fc71a94edc43a9c4b5e0ee3e63
hitesh05 <hitesh.goel05@gmail.com>
2021-06-14 15:33:59 +0530

    Only prints dir name and not path

--- Assignment_1/q5.sh
+++ Assignment_1/q5.sh
@@ -13,23 +13,41 @@ fi
 
 cd $x
 
-find . -type d | while read -r dir
+# find . -type d | while read -r dir
+# do 
+#     dirname=${dir##*/}
+#     printf "%s " "$dirname" >> temp.txt; 
+#     data=$(find "$dirname" -type f | wc -l); 
+#     data+=" file(s)"
+#     echo $data >> temp.txt
+# done
+
+find . -type d | while read -r dir 
 do 
-    printf "%s " "$dir" >> temp.txt; 
-    data=$(find "$dir" -type f | wc -l); 
-    data+=" file(s)"
-    echo $data >> temp.txt
+
+  dirname=${dir##*/}  
+    
+    no=`find "$dir" -type f | wc -l`
+
+echo "$dirname, $no file(s)" >> temp1.txt
 done
+sort -k 2nr temp1.txt >> temp2.txt
+sed -n '2,$p' temp2.txt
 
-echo
-echo "Directories: "
-awk '{ print $NF,$0 }' temp.txt | sort -k1,1 -nr | cut -f2- -d' '
+cat temp2.txt
 
-#rm -rf temp.txt
+# echo
+# echo "Directories: "
+# awk '{ print $NF,$0 }' temp.txt | sort -k1,1 -nr | cut -f2- -d' '
+
+rm -rf temp.txt
+rm -rf temp2.txt
 
 echo
 echo "Files: "
-#listing all files in a directory based on size:
-ls -lRS | grep '^[^d*]' | awk '{ print $9 }' | sort -k 2nr
+# #listing all files in a directory based on size:
+# #ls -lRS | grep '^[^d*]' | awk '{ print $9 }' | sort -k 2nr
+find . -type f -printf "%s %f\n" | sort -rn | awk '{print $2}'
+
 
 cd $y
