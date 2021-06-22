3d8b3aa0d08ecb77a8da9366237693ba06cbf358
hitesh05 <hitesh.goel05@gmail.com>
2021-06-22 09:54:49 +0530

    Added hover and sticky features in jscript

---
+++ Assignment_2/script.js
@@ -0,0 +1,31 @@
+document.getElementById("Home").addEventListener("mouseover", function () {
+    document.getElementById("Home").style.color = "yellow";
+});
+
+document.getElementById("Home").addEventListener("mouseout", function () {
+    document.getElementById("Home").style.color = "white";
+});
+
+document.getElementById("About").addEventListener("mouseover", function () {
+    document.getElementById("About").style.color = "yellow";
+});
+
+document.getElementById("About").addEventListener("mouseout", function () {
+    document.getElementById("About").style.color = "white";
+});
+
+document.getElementById("Exercises").addEventListener("mouseover", function () {
+    document.getElementById("Exercises").style.color = "yellow";
+});
+
+document.getElementById("Exercises").addEventListener("mouseout", function () {
+    document.getElementById("Exercises").style.color = "white";
+});
+
+document.getElementById("Contact").addEventListener("mouseover", function () {
+    document.getElementById("Contact").style.color = "yellow";
+});
+
+document.getElementById("Contact").addEventListener("mouseout", function () {
+    document.getElementById("Contact").style.color = "white";
+});
---
+++ copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf Assignment_2
+
+cp -r ../Assignment_2 Assignment_2
+
+git add .
