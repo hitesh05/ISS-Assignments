71787111a2f34dbaf58d61c9a436cd398d97cc37
hitesh05 <hitesh.goel05@gmail.com>
2021-06-25 22:44:28 +0530

    Added image-change feature

--- Assignment_2/script.js
+++ Assignment_2/script.js
@@ -49,3 +49,30 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+//IMAGE CHANGES ON CLICKING:
+count = 0;
+function changeimage() {
+    var imgelement = document.getElementById("changeimage")
+    if (count % 5 == 0) {
+        imgelement.src = "images/Img2.jpeg";
+        imgelement.alt = "Second image";
+    }
+    else if (count % 5 == 1) {
+        imgelement.src = "images/Img3.jpeg";
+        imgelement.alt = "Third image";
+    }
+    else if (count % 5 == 2) {
+        imgelement.src = "images/Img4.jpeg";
+        imgelement.alt = "Fourth image";
+    }
+    else if (count % 5 == 3) {
+        imgelement.src = "images/Img5.jpeg";
+        imgelement.alt = "Fifth image";
+    }
+    else if (count % 5 == 4) {
+        imgelement.src = "images/Img1.jpeg";
+        imgelement.alt = "First image";
+    }
+    count++;
+}
