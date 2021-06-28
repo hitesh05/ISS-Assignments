523bea78a42aa3a817815d7fa537d88b32a8682c
hitesh05 <hitesh.goel05@gmail.com>
2021-06-28 11:09:42 +0530

    Made changes to fit file structure

--- Assignment_2/js/script.js
+++ Assignment_2/js/script.js
@@ -55,23 +55,23 @@ count = 0;
 function changeimage() {
     var imgelement = document.getElementById("changeimage")
     if (count % 5 == 0) {
-        imgelement.src = "images/Img2.jpeg";
+        imgelement.src = "../img/Img2.jpeg";
         imgelement.alt = "Second image";
     }
     else if (count % 5 == 1) {
-        imgelement.src = "images/Img3.jpeg";
+        imgelement.src = "../img/Img3.jpeg";
         imgelement.alt = "Third image";
     }
     else if (count % 5 == 2) {
-        imgelement.src = "images/Img4.jpeg";
+        imgelement.src = "../img/Img4.jpeg";
         imgelement.alt = "Fourth image";
     }
     else if (count % 5 == 3) {
-        imgelement.src = "images/Img5.jpeg";
+        imgelement.src = "../img/Img5.jpeg";
         imgelement.alt = "Fifth image";
     }
     else if (count % 5 == 4) {
-        imgelement.src = "images/Img1.jpeg";
+        imgelement.src = "../img/Img1.jpeg";
         imgelement.alt = "First image";
     }
     count++;
